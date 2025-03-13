1745bef5d21f63bcdd6474561d9561a1dbe7ddcc
Fleur <f.luten@st.hanze.nl>
2025-03-13 10:38:12 +0100

    De website dynamisch gemaakt (opmaak opnieuw gebruiken enzo)

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+titels = ['Super Coole Website', 'UCSF Chimera', 'BLAST', 'Help', 'About Us']
+
 @app.route('/')
 def home():
-    return render_template('website.html')
+    return render_template(template_name_or_list = 'HOMEPAGE.html', titel = titels[0])
 
-@app.route('/blast_output')
-def blast():
-    return render_template('BLAST.html')
-
-@app.route('/visualisation')
+@app.route('/Chimera')
 def chimera():
-    return render_template('CHIMERA.html')
+    return render_template(template_name_or_list = 'CHIMERA.html', titel = titels[1])
 
-@app.route('/information')
-def about_us():
-    return render_template('ABOUT_US.html')
+@app.route('/BLAST')
+def blast():
+    return render_template(template_name_or_list = 'BLAST.html', titel = titels[2])
 
-@app.route('/help_information')
+@app.route('/Help')
 def help():
-    return render_template('HELP.html')
+    return render_template(template_name_or_list = 'HELP.html', titel = titels[3])
 
-
+@app.route('/AboutUs')
+def about_us():
+    return render_template(template_name_or_list = 'ABOUT_US.html', titel = titels[4])
 
 
 if __name__ == '__main__':
