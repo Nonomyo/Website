7beced2a31469d629af0e9b1d1c7f359d317f614
Fleur <f.luten@st.hanze.nl>
2025-03-24 10:08:26 +0100

    Paar kleine aanpassingen aan de opmaak van onze code

--- chimera.py
+++ chimera.py
@@ -7,7 +7,7 @@ def chimera_starten():
     # je kan hier ook bijvoorbeeld boven ook alleen: '1a0m' invullen als losse string
     # wat een build-in ID is invullen en dan opent hij die.
 
-    subprocess.run([r"C:\Program Files\ChimeraX 1.9\bin\chimerax.exe",insuline]) # hier opent hij chimera
+    subprocess.run([r"C:\Program Files\ChimeraX 1.9\bin\chimerax.exe", insuline]) # hier opent hij chimera
     # via MIJN path dit verschilt wss met iedereen maarja kijken we wel naar, tweede argument
     # is puur voor het openen van een bepaald protein. als je deze weghaald opent hij gewoon chimera
     # de start pagina
