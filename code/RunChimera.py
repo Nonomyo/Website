8a1ab91b9198ebb0628cbf6357700b6a23a1e30d
Spicy-BattleToaster <y.h.de.vos@st.hanze.nl>
2025-03-27 11:33:52 +0100

    made a working class. it is not integrated into the website yet.

--- code/RunChimera.py
+++ code/RunChimera.py
@@ -1,49 +1,52 @@
-from Bio.PDB import PDBList # For fetching the PDB file.
+""""
+RunChimera
+
+authors: Ype de Vos, Isa Bos, Naomy Schuppers, Fleur Luten.
+versie 1.
+date: 27-3-2025
+
+This script contains a class that asks the user to submit a pdb id. The class
+will use this pdb id to search through the pdb database for a file about the
+corresponding protein. This file, along with a csc file containing instruction,
+will be given to chimerax to generate an animation of the 3d structure of the protein.
+"""
+
+
+
+import subprocess
+
 
 class RunChimera():
-    def __init__(self, naam):
-        self.pdb_file = naam
+    def __init__(self, user_input):
+        self.chimera = r"C:\Program Files\Chimera 1.19\bin\chimera.exe"
+        self.user_input = user_input
 
 
 
-    def get_pdb(self):
+    def get_input(self, pdb_id):
         ''''
-        this function will contain import methods from the Bio.PDB import.
-        when given a pdb id, it wil fetch the corrosponding file from pdb.
+        This function will collect a pdb id as input and search through the pdb
+        database for the corresponding protein. next, this function will start
+        chimera to show an animation of the 3d structure of the protein.
 
-        :param PDB id: a 4 digit sequence containing both letters and numbers.
-        :return PDB file: a file containing a 3d structure of a protein.
+        :param pdb_id: a 4 digit sequence containing both letters and numbers.
+        :return protein structure: a 3d structure of the protein.
         '''
 
-
-    def read_pdb(self):
-        ''''
-        this function's importance is still questionable. if in some way to files
-        have to be read to extract data, this function will be your guy.
-
-        :param pdb file:
-        :return ?:
-        '''
-
-    def run_chimera(self):
-        ''''
-        this function will catch the pdb file from the get_pdb function or the ? from the
-        read_pdb function. the given information will be handed down to chimera to generate
-        the 3d structure.
-
-        :param pdb_file or ?: information that chimera needs to give back a 3d structure.
-        :return 3d structure: a 3d structure of a protein.
-        '''
-
-    def make_image(self):
-        ''''
-        this function will generate an image of the 3d structure. image format is not yet decided.
-
-        :param 3d structure: the 3d structure from chimera.
-        :return image: an image of the 3d structure.
-        '''
-
-#    def __str__(self):
-#        string = f'RunTool: {self.naam}' #voorbeeld
+        self.user_input = pdb_id
+        huidig_script = f"""open {pdb_id} 
+        movie record ; turn y 1 360 ; wait ; movie encode output video.mp4
+        """
+        script_path = "temp_chimera.cxc"
+        with open(script_path, "w") as script_file:
+            script_file.write(huidig_script)
+        subprocess.run([r"C:\Program Files\ChimeraX 1.9\bin\ChimeraX.exe", '--script', script_path])
 
 
+    def __str__(self):
+
+        return f'running chimera with pdb_id: {[self.get_input]}' #voorbeeld
+
+
+test = RunChimera(input)
+test.get_input(input('prot id; '))
